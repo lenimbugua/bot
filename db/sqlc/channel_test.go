0f5bfec5d5153ce18cfe981d8b6b470855e63be8
lenimbugua <lenimbugua@gmail.com>
2022-10-26 12:05:23 +0300

    added channel tests

--- db/sqlc/channel_test.go
+++ db/sqlc/channel_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -34,4 +35,55 @@ func TestGetChannel(t *testing.T) {
 	require.Equal(channel1.Name, channel2.Name)
 	require.WithinDuration(channel1.CreatedAt, channel2.CreatedAt, time.Second)
 	require.WithinDuration(channel1.UpdatedAt, channel2.UpdatedAt, time.Second)
+}
+
+func TestUpdateChannel(t *testing.T) {
+	require := require.New(t)
+
+	channel := createRandomChannel(t)
+
+	name := util.RandomString(6)
+	arg := UpdateChannelParams{
+		Name: sql.NullString{String: name, Valid: true},
+		ID:   channel.ID,
+	}
+	updatedChannel, err := testQueries.UpdateChannel(context.Background(), arg)
+	require.NoError(err)
+	require.NotEmpty(updatedChannel)
+	require.NotEqual(channel.Name, updatedChannel.Name)
+	require.Equal(name, updatedChannel.Name)
+	require.NotEqual(channel.UpdatedAt, updatedChannel.UpdatedAt)
+	require.WithinDuration(channel.UpdatedAt, updatedChannel.UpdatedAt, 30*time.Second)
+	require.WithinDuration(channel.CreatedAt, updatedChannel.CreatedAt, time.Second)
+}
+
+func TestDeleteChannel(t *testing.T) {
+	require := require.New(t)
+	channel := createRandomChannel(t)
+
+	err := testQueries.DeleteChannel(context.Background(), channel.ID)
+	require.NoError(err)
+	channel1, err := testQueries.GetChannel(context.Background(), channel.Name)
+	require.Error(err)
+	require.EqualError(err, sql.ErrNoRows.Error())
+	require.Empty(channel1)
+}
+
+func TestListChannels(t *testing.T) {
+	require := require.New(t)
+	for i := 0; i < 10; i++ {
+		createRandomChannel(t)
+	}
+	arg := ListChannelsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	channels, err := testQueries.ListChannels(context.Background(), arg)
+	require.NoError(err)
+	require.Equal(len(channels), 5)
+
+	for _, channel := range channels {
+		require.NotEmpty(channel)
+	}
 }
